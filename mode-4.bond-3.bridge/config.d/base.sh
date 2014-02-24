408c68ea0ba7ffc927a6d10e5fc5d94ff453c595
Masahito Yoshida <masahito@axsh.net>
2014-02-24 15:06:09 +0900

    add mode-4.bond-3.bridge.

---
+++ mode-4.bond-3.bridge/config.d/base.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -x
+
+mode=4
+
+## functions
+
+function install_bonding_conf() {
+  local master_if=${1:-bond0}
+
+  local bonding_conf_path=/etc/modprobe.d/bonding.conf
+  if [[ ! -f ${bonding_conf_path} ]]; then
+    : > ${bonding_conf_path}
+  fi
+
+  local bond_entry="alias ${master_if} bonding"
+  egrep -q -w "^${bond_entry}" ${bonding_conf_path} || {
+    echo ${bond_entry} >> ${bonding_conf_path}
+  }
+}
+
+function gen_ifcfg_path() {
+  local device=${1:-eth0}
+  local ifcfg_path=/etc/sysconfig/network-scripts/ifcfg
+
+  echo ${ifcfg_path}-${device}
+}
+
+function render_ifcfg_bond_master() {
+  local master_if=${1:-bond0}
+
+  cat <<-EOS
+	DEVICE=${master_if}
+	ONBOOT=yes
+	BOOTPROTO=none
+	BONDING_OPTS="mode=${mode:-1} miimon=${miimon:-100} updelay=${updelay:-500} fail_over_mac=1"
+	EOS
+}
+
+function render_ifcfg_bond_slave() {
+  local master_if=${1:-bond0} slave_if=${2:-eth0}
+
+  cat <<-EOS
+	DEVICE=${slave_if}
+	BOOTPROTO=none
+	ONBOOT=yes
+	MASTER=${master_if}
+	SLAVE=yes
+	EOS
+}
+
+function install_ifcfg_file() {
+  local device=${1:-eth0}
+
+  tee $(gen_ifcfg_path ${device}) </dev/stdin
+}
+
+function install_ifcfg_bond_master() {
+  local master_if=${1:-bond0}
+
+  render_ifcfg_bond_master ${master_if} | install_ifcfg_file ${master_if}
+}
+
+function install_ifcfg_bond_slave() {
+  local master_if=${1:-bond0} slave_if=${2:-eth0}
+
+  render_ifcfg_bond_slave ${master_if} ${slave_if} | install_ifcfg_file ${slave_if}
+}
+
+function install_ifcfg_bond_map() {
+  local master_if=${1:-bond0}; shift
+  local slave_ifs=${@} slave_if
+
+  install_bonding_conf      ${master_if}
+  install_ifcfg_bond_master ${master_if}
+  for slave_if in ${slave_ifs}; do
+    install_ifcfg_bond_slave ${master_if} ${slave_if}
+  done
+}
+
+function render_ifcfg_bridge() {
+  local master_if=${1:-br0}
+
+  cat <<-EOS
+	DEVICE=${master_if}
+	TYPE=Bridge
+	BOOTPROTO=none
+	ONBOOT=yes
+	EOS
+}
+
+function install_ifcfg_bridge_map() {
+  local master_if=${1:-br0} slave_if=${2:-eth0}
+
+  render_ifcfg_bridge ${master_if} | install_ifcfg_file ${master_if}
+
+  local slave_ifcfg_path=$(gen_ifcfg_path ${slave_if})
+  if [[ ! -f ${slave_ifcfg_path} ]]; then
+    : > ${slave_ifcfg_path}
+  fi
+
+  local bridge_entry="BRIDGE=${master_if}"
+  egrep -q -w "^${bridge_entry}" ${slave_ifcfg_path} || {
+    echo ${bridge_entry} >> ${slave_ifcfg_path}
+  }
+}
+
+## bond0: eth1,eth2
+## bond1: eth3,eth4
+## bond2: eth5,eth6
+
+install_ifcfg_bond_map bond0 eth1 eth2
+install_ifcfg_bond_map bond1 eth3 eth4
+install_ifcfg_bond_map bond2 eth5 eth6
+
+install_ifcfg_bridge_map br0 bond0
+install_ifcfg_bridge_map br1 bond1
+install_ifcfg_bridge_map br2 bond2
+
+##
+
+# /etc/init.d/network restart
---
+++ mode-4.bond-3.bridge/config.d/node01.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -x
+
+function append_networking_param() {
+  local device=${1:-eth0}; shift; eval local ${@}
+
+  cat <<-EOS | tee -a /etc/sysconfig/network-scripts/ifcfg-${device}
+	IPADDR=${ip}
+	NETMASK=${mask}
+	EOS
+}
+
+ip4=43
+
+append_networking_param br0 ip=10.126.5.${ip4} mask=255.255.255.0
+append_networking_param br1 ip=10.126.6.${ip4} mask=255.255.255.0
+append_networking_param br2 ip=10.126.7.${ip4} mask=255.255.255.0
+
+##
+
+/etc/init.d/network restart
