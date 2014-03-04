73eed48395310d889926b2171a4df0773d55ed44
Masahito Yoshida <masahito@axsh.net>
2014-03-04 18:08:39 +0900

    add mode-4.bond-3.vlan.bridge.

---
+++ mode-4.bond-3.vlan.bridge/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+function yum() {
+  $(type -P yum) --disablerepo=updates "${@}"
+}
+
+# Add installation packages ...
+addpkgs="
+ bridge-utils
+ vconfig
+"
+
+if [[ -n "$(echo ${addpkgs})" ]]; then
+  yum install -y ${addpkgs}
+fi
